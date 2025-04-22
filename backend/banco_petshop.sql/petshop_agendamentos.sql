4a248f8c52fcff25e3e90c0b06e5abb92cec02af
du341 <du341du2004@hotmail.com>
2025-04-22 19:48:05 -0300

    Update petshop_agendamentos.sql

--- backend/banco_petshop.sql/petshop_agendamentos.sql
+++ backend/banco_petshop.sql/petshop_agendamentos.sql
@@ -1,27 +1,3 @@
--- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: petshop
--- ------------------------------------------------------
--- Server version	8.0.41
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `agendamentos`
---
-
-DROP TABLE IF EXISTS `agendamentos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `agendamentos` (
   `id` int NOT NULL AUTO_INCREMENT,
   `usuario_id` int NOT NULL,
@@ -35,25 +11,11 @@ CREATE TABLE `agendamentos` (
   KEY `usuario_id` (`usuario_id`),
   CONSTRAINT `agendamentos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `agendamentos`
---
 
 LOCK TABLES `agendamentos` WRITE;
 /*!40000 ALTER TABLE `agendamentos` DISABLE KEYS */;
 INSERT INTO `agendamentos` VALUES (1,1,'Rex editado','Golden Retriever','2025-09-20','16:00:00','Banho e escovação completa','1745331797446-369364677.jpg'),(3,2,'dhaudhai','dihiaihdioad','2025-04-24','03:40:00','dahduhadiamda','1745334278159-923828730.jpeg'),(4,2,'ozzy','doajkdoajmda','2025-04-19','11:12:00','212sasdadw','1745336029378-199348774.jpg'),(5,2,'ozzy','Vira lata','2025-04-24','11:00:00','medroso com barulho','1745336090256-409994210.jpg'),(6,1,'ozzy','golden ','2025-04-24','11:00:00','...','1745337856246-704222552.jpg'),(7,3,'odhada','dadadwda','2025-04-24','11:00:00','dadwdadada','1745338549791-200764410.jpg'),(8,2,'dadwd','adfwdfadw','2025-04-24','11:00:00','dawdawd','1745338705620-779280096.jpg');
 /*!40000 ALTER TABLE `agendamentos` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-04-22 13:47:07
